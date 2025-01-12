7e417f20101661a72c611763e5a7ed17dc2e945e
Yuri Konev <crazy_clown93@mail.ru>
2025-01-12 22:21:28 +0300

    Example 2

---
+++ HomeWork_1.2/HomeWork_1.2/HomeWork_1.2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    double num1, num2;
+
+    std::cout << "Введите первое число: ";
+    std::cin >> num1;
+
+    std::cout << "Введите второе число: ";
+    std::cin >> num2;
+
+    std::cout << "-----Проверяем-----" << std::endl;
+
+    // Сравниваем числа и выводим результат
+    if (num1 < num2) {
+        std::cout << "Наименьшее число: " << num1 << std::endl;
+    }
+    else if (num1 > num2) {
+        std::cout << "Наименьшее число: " << num2 << std::endl;
+    }
+    else {
+        std::cout << "Числа равны!" << std::endl;
+    }
+
+    return 0;
+}
