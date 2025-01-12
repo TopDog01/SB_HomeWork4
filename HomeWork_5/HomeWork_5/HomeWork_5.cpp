2b39176c57900765927b6f8c1104246ae08319f0
Yuri Konev <crazy_clown93@mail.ru>
2025-01-12 22:56:39 +0300

    Task5

---
+++ HomeWork_5/HomeWork_5/HomeWork_5.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    int num1, num2;
+
+    std::cout << "Введите первое число: ";
+    std::cin >> num1;
+
+    std::cout << "Введите второе число: ";
+    std::cin >> num2;
+
+    // Проверяем, что второе число не равно нулю
+    if (num2 == 0) {
+        std::cout << "Ошибка: деление на ноль невозможно!" << std::endl;
+        return 1; // Завершаем программу с кодом ошибки
+    }
+
+    std::cout << "-----Проверяем-----" << std::endl;
+
+    // Проверяем, делится ли num1 на num2 без остатка
+    if (num1 % num2 == 0) {
+        std::cout << "Да, " << num1 << " делится на " << num2 << " без остатка!" << std::endl;
+    }
+    else {
+        std::cout << "Нет, " << num1 << " не делится на " << num2 << " без остатка." << std::endl;
+    }
+
+    return 0;
+}
