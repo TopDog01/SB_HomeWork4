b48c4367d483e3779f1f488c9d80968227b2e871
Yuri Konev <crazy_clown93@mail.ru>
2025-01-12 22:10:17 +0300

    Examlpe 1

---
+++ HomeWork/HomeWork/HomeWork.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+int main() {
+
+    setlocale(LC_ALL, "Russian");
+
+    double num1, num2;
+
+    std::cout << "Введите первое число: ";
+    std::cin >> num1;
+
+    std::cout << "Введите второе число: ";
+    std::cin >> num2;
+
+    std::cout << "-----Проверяем-----" << std::endl;
+
+    // Находим и выводим наименьшее число
+    if (num1 < num2) {
+        std::cout << "Наименьшее число: " << num1 << std::endl;
+    }
+    else {
+        std::cout << "Наименьшее число: " << num2 << std::endl;
+    }
+
+    return 0;
+}
