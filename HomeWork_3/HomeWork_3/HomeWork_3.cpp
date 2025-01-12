ecc318723beac81fec58da540164db06f2e6a490
Yuri Konev <crazy_clown93@mail.ru>
2025-01-12 22:41:31 +0300

    Task3

---
+++ HomeWork_3/HomeWork_3/HomeWork_3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    int x;
+
+    std::cout << "Введите число: ";
+    std::cin >> x;
+
+    // Вычисляем остаток от деления x на 2
+    int remainder = x % 2;
+
+    // Проверяем, четное ли число
+    if (remainder == 0) {
+        std::cout << "Число " << x << " является четным." << std::endl;
+    }
+    else {
+        std::cout << "Число " << x << " является нечетным." << std::endl;
+    }
+
+    return 0;
+}
