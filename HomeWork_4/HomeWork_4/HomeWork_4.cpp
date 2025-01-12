85e189f800ba55a02bb651c95fc17d99f269a9c5
Yuri Konev <crazy_clown93@mail.ru>
2025-01-12 22:49:26 +0300

    Task4

---
+++ HomeWork_4/HomeWork_4/HomeWork_4.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    int experiencePoints;
+
+    std::cout << "Введите число очков опыта: ";
+    std::cin >> experiencePoints;
+
+    // Проверка на корректность ввода
+    if (std::cin.fail() || experiencePoints < 0) {
+        std::cout << "Ошибка: введите неотрицательное целое число." << std::endl;
+        return 1; // Завершаем программу с кодом ошибки
+    }
+
+    std::cout << "-----Считаем-----" << std::endl;
+
+    int level = 1; // Начальный уровень
+
+    // Определяем уровень на основе очков опыта
+    if (experiencePoints >= 5000) {
+        level = 5;
+    }
+    else if (experiencePoints >= 2500) {
+        level = 4;
+    }
+    else if (experiencePoints >= 1000) {
+        level = 3;
+    }
+
+    std::cout << "Ваш уровень: " << level << std::endl;
+
+    return 0;
+}
